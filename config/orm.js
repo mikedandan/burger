14a580f220b4729a4635ebe711431eb6f993642c
mikedandan <mikedandan2@gmail.com>
2019-02-24 17:58:06 -0800

    created connection.js, orm.js and burger.js code

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,88 @@
+//import the mysql connection
 var connection = require("../config/connection.js");
+
+
+// Helper function for generating '?' to use in MySQL statements
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//This function pushes 'key value'='value of key' and returns it as a string
+function objToSql(myObj) {
+    var arr = [];
+
+    for (var key in myObj) {
+        arr.push(key + "=" + myObj[key]);
+    }
+
+    return arr.toString();
+}
+
+var orm = {
+    // Function that returns all table entries
+    selectAll: function (tableInput, cb) {
+        // Construct the query string that returns all rows from the target table
+        var queryString = "SELECT * FROM " + tableInput + ";";
+
+        // Perform the database query that we built into queryString and return the results in...result!
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // The callback function returns the result
+            cb(result);
+        });
+    },
+
+    // Function that inserts one row into a table. "cb" is, as usual, our trusty callback function
+    insertOne: function (table, cols, vals, cb) {
+        // Construct a query to insert one row into the table
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // The callback function returns the result
+            cb(result);
+        });
+    },
+
+    // Function to update one burger in the table
+    updateOne: function (table, objColVals, condition, cb) {
+        // Construct the query string that updates a single entry in the target table
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // The callback function returns the result
+            cb(result);
+        });
+    }
+};
+
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+var orm = require("../config/orm.js");
+
+// Create a hburger object
+var burger = {
+    // Select all burger entries
+    selectAll: function(cb) {
+      orm.selectAll('burgers', function(res) {
+        cb(res);
+      });
+    },
+  
+    // This function calls the ORM single insert function.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne('burgers', cols, vals, function(res) {
+        cb(res);
+      });
+    },
+  
+    // objColVals is an object specifying columns as object keys with associated values
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne('burgers', objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for use by burgers_controller.js
+  module.exports = burger;
