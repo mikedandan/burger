10a1c28305ad031bdf7a051cfbc6f5ecd623376a
mikedandan <mikedandan2@gmail.com>
2019-02-24 19:13:19 -0800

    updated server.js, fixed routes and added images

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var routes = require('./controllers/burger_controllers');
 var app = express();
 
 // Set the port of our application
@@ -8,10 +8,12 @@ var PORT = process.env.PORT || 8080;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
+// app.use('/', routes); //use the routes provided in burgers_controller.js
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/',routes);
 
 var exphbs = require("express-handlebars");
 
