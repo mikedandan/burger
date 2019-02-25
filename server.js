acf2fded9ba7ff0e0dafeefc7661f866d22a1114
mikedandan <44042639+mikedandan@users.noreply.github.com>
2019-02-25 15:53:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+
 // app.use('/', routes); //use the routes provided in burgers_controller.js
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
